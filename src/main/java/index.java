a6aa83d2b902cbbe0b9daceef217f2525d04ce3f
Your Name <you@example.com>
2021-03-22 22:55:13 +0530

    done

--- src/main/java/index.java
+++ src/main/java/index.java
@@ -4,54 +4,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class index {
 
     static JFrame frame = new JFrame();
 
     public static void main(String[] arg){
-        frame.setSize(400,400);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
 
-        JLabel jLabel = new JLabel("Setting up environments...",SwingConstants.CENTER);
-        jLabel.setSize(50,50);
-        frame.add(jLabel);
 
-        jLabel.setText("Getting os");
+        System.out.println(System.getProperty("user.dir"));
 
-        jLabel.setText("Detected "+getOperatingSystem());
-
-        System.out.println(getOperatingSystem());
-
-        jLabel.setText("installing dependencies");
         try {
-            Process prs= Runtime.getRuntime().exec("git commit -m 'intial init'");
-            printResults(prs);
-            jLabel.setText("done");
-        } catch (IOException e) {
+
+            String path = "file://"+ System.getProperty("user.dir").replace(" ","%20")+"/src/main/java/index.html";
+
+
+            System.out.println(path);
+            Desktop.getDesktop().browse(new URL(path).toURI());
+        } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
         }
 
 
     }
 
-    public static Process execCmd(String cmd) throws java.io.IOException {
-        String[] args = new String[] {"/bin/bash", "-c", "java -version", "with", "args"};
-        return new ProcessBuilder(args).start();
 
-    }
-    public static void printResults(Process process) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String line = "";
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
-        }
-    }
-
-    public static String getOperatingSystem() {
-        return System.getProperty("os.name");
-    }
 }
